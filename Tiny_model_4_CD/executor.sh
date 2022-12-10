d79371ed366cb4fd45ac0774655218dfd626d0a8
cedricb13579 <cedricb@vt.edu>
2022-12-09 20:17:06 -0500

    executor script draft

--- Tiny_model_4_CD/executor.sh
+++ Tiny_model_4_CD/executor.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ "$1" == "test_single" ]; then
+	python3 test_single_img.py test_1_4.png model_8.pth
+fi
 if [ "$1" == "test" ]; then
-    echo test
+	python3 test_ondata.py --datapath LEVIR-CD-256 --modelpath model_8.pth	
 fi
 if [ "$1" == "train" ]; then
-    python3 training.py --datapath LEVIR-CD-256 --log-path model_logs
+	python3 training.py --datapath LEVIR-CD-256 --log-path model_logs
+fi
+if [ "$1" == "download" ]; then
+	wget https://www.dropbox.com/s/h9jl2ygznsaeg5d/LEVIR-CD-256.zip
+	unzip LEVIR-CD-256.zip
 fi
